030a786ba038279db0528efe3ad36d12804c7959
nghianguyenbbv <nghia2nguyen@bbv.vn>
2023-11-09 09:40:11 +0700

    add project

---
+++ renovateWithGithubApp/src/main/java/com/example/renovatewithgithubapp/RenovateWithGithubAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.renovatewithgithubapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovateWithGithubAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RenovateWithGithubAppApplication.class, args);
+  }
+
+}
---
+++ renovateWithGithubApp/src/test/java/com/example/renovatewithgithubapp/RenovateWithGithubAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renovatewithgithubapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovateWithGithubAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
